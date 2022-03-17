193efc51fe9ef39f3da58205b22592d09a9d0889
kapepa <kapepayx@gmail.com>
2022-03-18 00:08:44 +0200

    18.03

---
+++ client/components/header/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import style from './header.module.scss';
+
+const Header: NextPage = () => {
+  return (
+    <div className={style.header}>
+      Header
+    </div>
+  )
+};
+
+export default Header;

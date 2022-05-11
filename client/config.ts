d4a2da7e48a919810aebc2c1bd51ff341cef9306
kapepa <kapepayx@gmail.com>
2022-05-12 00:26:05 +0300

    12.05

--- client/config.ts
+++ client/config.ts
@@ -1,6 +1,6 @@
 const config = {
   url: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:5000' : 'http://127.0.0.1:5000',
   api: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:5000' : 'http://nestjs:5000',
-}
+};
 
 export default config;
